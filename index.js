720a86c9bdcd85ffd3bbbd8b95e19c2d391a9321
sanchez-matias <matias.emanuel.sanchez@hotmail.com>
2025-07-07 11:26:50 -0300

    Cambiar a una URL independiente de la VPN

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const createWindow = () => {
         // icon: path.join(__dirname, 'assets/icon.png'),
     });
 
-    window.loadURL('https://framework.xrp.dev/?sid=39f995f287bd7b92f43a152dbe235a9044a6477a57db429fc6bf30928ea8386bc7c6da2c806cac03295802ded5b603cade8c27ddff5c5d53f03fb04146200a83&psid=bc2cNbIrsxcE4RgE4eCt'); // Este debe ser el html del contenido
+    window.loadURL('https://prx.xrp.com.ar');
 }
 
 app.whenReady().then(() => {
